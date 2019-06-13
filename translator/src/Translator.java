24c960574863042fbbf8c8aabf3b9b20d9d228e5
Rodrigo Bedoya <rodrigo.bedoya@utec.edu.pe>
2019-06-13 12:19:18 -0500

    reduced translate func parameters

--- translator/src/Translator.java
+++ translator/src/Translator.java
@@ -8,14 +8,14 @@ import java.net.URLEncoder;
 public class Translator {
     public static void main(String[] args) throws IOException {
         String text = "Him rendered may attended concerns jennings reserved now. Sympathize did now preference unpleasing mrs few. Mrs for hour game room want are fond dare. For detract charmed add talking age. Shy resolution instrument unreserved man few. She did open find pain some out. If we landlord stanhill mr whatever pleasure supplied concerns so. Exquisite by it admitting cordially september newspaper an. Acceptance middletons am it favourable. It it oh happen lovers afraid. \n";
-        System.out.println("Translated text: " + translate("en", "es", text));
+        System.out.println("Translated text: " + translate(text));
     }
 
-    private static String translate(String langFrom, String langTo, String text) throws IOException {
+    private static String translate(String text) throws IOException {
         String urlStr = "https://script.google.com/macros/s/AKfycbwoewR66GN4nBRmwKUzAz7dlyOZ2MIN9T5pAsUk56puPV7P0xJa/exec" +
                 "?q=" + URLEncoder.encode(text, "UTF-8") +
-                "&target=" + langTo +
-                "&source=" + langFrom;
+                "&target=" + "es" +
+                "&source=" + "en";
         URL url = new URL(urlStr);
         StringBuilder response = new StringBuilder();
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
